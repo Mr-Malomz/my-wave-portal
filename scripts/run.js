df35a64262820cf2c6ead1f4a70d66b22f4ec8c9
demola malomo <malomo.alaba@yahoo.com>
2021-09-21 14:29:40 +0100

    sending waves

---
+++ scripts/run.js
@@ -0,0 +1,34 @@
+async function main() {
+  const [owner, randoPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randoPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+}
+
+const runMain = () => {
+  try {
+    main().then(() => process.exit(0));
+    process.emit(0);
+  } catch (error) {
+    console.error(error);
+    process.emit(1);
+  }
+};
+
+runMain();
